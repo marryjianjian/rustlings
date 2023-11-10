ae797d4bc4433cc1604c04a64e463e527ad6239b
marryjianjian <logjian@gmail.com>
2023-11-10 10:27:34 +0800

    feat: complete 0-17 exercises

--- exercises/20_threads/threads1.rs
+++ exercises/20_threads/threads1.rs
@@ -8,7 +8,6 @@
 // Execute `rustlings hint threads1` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
 use std::thread;
 use std::time::{Duration, Instant};
@@ -26,7 +25,10 @@ fn main() {
 
     let mut results: Vec<u128> = vec![];
     for handle in handles {
-        // TODO: a struct is returned from thread::spawn, can you use it?
+        match handle.join() {
+            Ok(n) => results.push(n),
+            Err(e) => {},
+        }
     }
 
     if results.len() != 10 {
